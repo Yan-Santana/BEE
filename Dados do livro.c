49465c86d2ff86268e62b3366b7812020980ae1a
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 20:15:02 -0400

    Update Dados do livro.c

--- Dados do livro.c
+++ Dados do livro.c
@@ -1,18 +1,13 @@
 /*PROGRAMA PARA CADASTRO E CONSULTA DE LIVROS*/
 #include <stdio.h>
-// constantes
 #define MAX 30
 
-// definicao de tipos
-struct tLivro
-{
+struct tLivro {
 	char titulo[MAX], autor[MAX];
 	int ano;
 };
 
-// funcao para cadastrar um livro
-void cadLivro (int &n, tLivro v[])
-{
+void cadLivro (int &n, tLivro v[]) {
 	printf("\nTitulo: ");
 	scanf(" %[^\n]", v[n].titulo);
 	printf("Autor: ");
@@ -20,10 +15,9 @@ void cadLivro (int &n, tLivro v[])
 	printf("Ano: ");
 	scanf("%d", &v[n].ano);
 
-	// incrementando o numero de livros cadastrados
 	n++;
 }
-// funcao para imprimir os dados dos livros de um certo ano informado pelo usuario
+
 void listaPorAno(int ano, int n, tLivro v[]){
     int aux = 0;
 
@@ -33,30 +27,24 @@ void listaPorAno(int ano, int n, tLivro v[]){
             aux++;
         }
     }
-if(aux == 0)
-printf("Nenhum livro dessa data encontrado!");
+	if(aux == 0)
+	   printf("Nenhum livro dessa data encontrado!");
 }
 
-/* funcao para imprimir os dados dos livros cujo nome do autor comeca com uma letra
-informada pelo usuario*/
-void listaPorLetra(char letra, int n, tLivro v[])
-    {
+void listaPorLetra(char letra, int n, tLivro v[]) {
     int aux = 0;
     for(int i=0; i<n; i++){
         if(letra == v[i].autor[0]){
             printf("%s, %s, %d.\n", v[i].titulo, v[i].autor, v[i].ano);
             aux++;
             }
-        }
+	}
     if(aux == 0)
     printf("-1");
+}
 
-    }
 
-
-// funcao para imprimir o menu
-void menu()
-{
+void menu() {
 	printf("\n\n1 - Cadastrar um livro");
 	printf("\n2 - Listar livros por ano");
 	printf("\n3 - Listar por inicial do nome do autor");
@@ -65,24 +53,19 @@ void menu()
 	printf("\nDigite sua opcao: ");
 }
 
-// funcao para imprimir todos os cadastrados
-void imprimir(int n, tLivro v[]);
-{
+void imprimir(int n, tLivro v[]){
 	printf("\n\n");
 	for(int i = 0; i < n; i++)
 		printf("%s, %s, %d.\n", v[i].titulo, v[i].autor, v[i].ano);
 
 	printf("\n\n");
 }
-// funcao principal
-int main()
-{
+int main() {
 	tLivro vetor[MAX];
-	int n = 0, ano;			// qtde de livros cadastrados
+	int n = 0, ano;			
 	int opcao;
-    char letraa;
+    	char letraa;
 	do{
-		// chamando a funcao p/ imprimir menu
 		menu();
 		scanf("%d", &opcao);
 
@@ -104,10 +87,5 @@ int main()
 			imprimir(n, vetor);
 
 	}while(opcao != 5);
-
 	return 0;
 }
-
-// -----------------------------------------------------
-
-
