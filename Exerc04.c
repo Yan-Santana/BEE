9b5f2a7358ce500bd14ee2922764927b10445d14
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 20:16:54 -0400

    Update and rename Exerc04-lista02.c to Exerc04.c

--- Exerc04.c
+++ Exerc04.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
 #define MAX 100
-int main()
-{
+int main() {
     int L[MAX],i, cont= 0, tam, maior;
-
-    //entrada de dados para saber o tamanho do vetor
+    
     scanf(" %d", &tam);
-    //Laço que irá preencher os dados do vetor
-    for(i=1; i<=tam; i++){
+    for(i=1; i<=tam; i++) {
         scanf(" %d", &L[i]);
     }
     cont = 0;
     maior = L[0];
-    for(i=1; i<=tam; i++){
+    for(i=1; i<=tam; i++) {
         if(maior <= L[i] && i != 0)
             cont = 1;
     }
-    // Verificação
+    
     if(cont == 0){
         printf("true");
         }
     else{
         printf("false");
         }
-
-
-
     return 0;
 }
