3735fd484e410e3bea4f90616c10c64ad329f3e5
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 20:16:00 -0400

    Update Estrutura for.c

--- Estrutura for.c
+++ Estrutura for.c
@@ -1,32 +1,21 @@
 // Programa: somaimpar.cpp
-// Este programa lê dois números inteiros,
+// Este programa lÃª dois nÃºmeros inteiros,
 // inicio e fim e calcula a soma dos numeros
-// inteiros ímpares entre inicio e fim,
+// inteiros Ã­mpares entre inicio e fim,
 // inclusive inicio e fim
-// Declaração das biliotecas utilizadas
+// DeclaraÃ§Ã£o das biliotecas utilizadas
 #include<stdio.h>
 
-// início da função principal
 int main() {
-// declaração das variáveis utilizadas
     int inicio, fim, soma;
     soma = 0;
-
-// Passo 1. Leia a entrada e inicialize soma
+    
     scanf("%d%d", &inicio, &fim);
 
-// Passo 2. Calcule a soma em [inicio,fim]
-    for(inicio; inicio <= fim; inicio++){
+    for(inicio; inicio <= fim; inicio++) {
         if (inicio % 2 != 0){
         soma += inicio;}
     }
-
-
-// Passo 2.1. Verifique se inicio é impar
-
-// Passo 3. Imprima o resultado
    printf("%d\n", soma);
-
    return 0;
-
-} // fim main
+} 
