157689a7bec311d46fb7a2d44d00ee0d7503b44e
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 20:25:24 -0400

    Update and rename Considere o pseudocódigo da seguinte função recursiva.cpp to função recursiva.cpp

--- função recursiva.cpp
+++ função recursiva.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAX 20
 
-int conta(int n, int *w, int k){
+int conta(int n, int *w, int k) {
     if(n == 1 && w[0] == k)
         return 1;
 
@@ -12,18 +12,15 @@ int conta(int n, int *w, int k){
         return (k == *w) + conta(n-1, w+1, k);
 }
 
-int main(){
+int main() {
     int n, w[MAX],k,i;
-
-
     scanf("%d", &n);
-
-    for(i=0; i<n; i++){
+    
+    for(i=0; i<n; i++) 
         scanf("%d\n", &w[i]);
-    }
+    
     scanf("%d", &k);
-
     printf("%d\n",conta(n, w, k));
-
+    
     return 0;
 }
