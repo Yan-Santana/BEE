6dece4d3cad0a378eba11880032befd117216c11
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 20:07:26 -0400

    Update Agenda de compromissos.c

--- Agenda de compromissos.c
+++ Agenda de compromissos.c
@@ -1,35 +1,28 @@
 /* Programa para simular uma agenda de compromissos
-Faça um programa para simular uma agenda de compromissos e:
-• Crie e leia um vetor de 5 estruturas de dados com: compromisso (máximo 60 letras) e data, nesta ordem. A data deve ser outra estrutura de dados contendo dia, mês e ano.
-• Leia dois inteiros M e A e mostre todos os compromissos do mês M do ano A. Repita o procedimento até ler M = 0.
+FaÃ§a um programa para simular uma agenda de compromissos e:
+â€¢ Crie e leia um vetor de 5 estruturas de dados com: compromisso (mÃ¡ximo 60 letras) e data, nesta ordem. A data deve ser outra estrutura de dados contendo dia, mÃªs e ano.
+â€¢ Leia dois inteiros M e A e mostre todos os compromissos do mÃªs M do ano A. Repita o procedimento atÃ© ler M = 0.
 Dica: utilize  scanf("%d/%d/%d", &variavel_dia, &variavel_mes, &variavel_ano); para leitura formatada da data.*/
 #include <stdio.h>
 #define MAX 61
 
-struct comp{ // Registro para guardar a string(texto) do compromisso
-    char compromisso[MAX]; // Até 60 caracteres
+struct comp{ 
+    char compromisso[MAX]; 
 };
 
-struct data{ // Registro para guardar a data(dia, mês, ano).
+struct data{ 
     int dia, mes, ano;
 };
 
 int main(){
     comp dadosCOMP[5];
     data DT[5];
-    int Mcial, Acial, Mfinal, Afinal; // M == Mês; A == Ano;
+    int Mcial, Acial, Mfinal, Afinal; 
 
-    for(int i=0; i<5; i++){ //Laço para preencher os dados do Compromisso e datas
-
-        //printf("Compromisso: ");
+    for(int i=0; i<5; i++){ 
         scanf(" %[^\n]", dadosCOMP[i].compromisso);
-        //printf("Data dia/mes/ano");
         scanf ("%d/%d/%d", &DT[i].dia, &DT[i].mes, &DT[i].ano);
-        //printf("\n");
-
     }
-    //printf("Data P/ pesquisa (MES/Ano): ");
-
     scanf("%d/%d\n", &Mcial, &Acial); // Data p/ filtragem de Compromissos
     scanf("%d/%d\n", &Mfinal, &Afinal); // Data p/ filtragem de Compromissos
 
@@ -40,9 +33,6 @@ int main(){
     for(int i=0; i<5; i++){
         if(Mfinal == DT[i].mes && Afinal == DT[i].ano)
             printf("%s\n", dadosCOMP[i].compromisso);
-
      }
-
-
     return 0;
 }
